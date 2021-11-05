e3a580ddd30c246943ae72e85765b35d60e37157
Saulo Freire <gbsaulo@gmail.com>
2021-11-05 09:52:44 -0300

    fix: Ionic config.xml file path search

--- ios/Runner/NativeFlutterCoordinators/Ionic/IonicViewController.swift
+++ ios/Runner/NativeFlutterCoordinators/Ionic/IonicViewController.swift
@@ -15,4 +15,26 @@ class IonicViewController:MainViewController {
     func goToFlutter(_ sender: Any){
         coordinatorDelegate?.navigateToFlutter()
     }
+    
+    override func configFilePath() -> String? {
+        var path = self.configFile ?? "config.xml";
+        if (!NSString(string: path).isAbsolutePath) {
+            let podBundle = Bundle(for: MainViewController.self)
+            let resourceURL = podBundle.resourceURL?.appendingPathComponent("ionicAppOne.bundle")
+            let resourceBundle = Bundle(url: resourceURL!)
+            
+            guard let absolutePath = resourceBundle!.path(forResource: path, ofType: nil) else {
+                assertionFailure("\(path) not found in the pod bundle!")
+                return nil
+            }
+            path = absolutePath
+        }
+        
+        if(!FileManager.default.fileExists(atPath: path)) {
+            assertionFailure("\(path) does not exist. Please run cordova-ios/bin/cordova_plist_to_config_xml path/to/project.")
+            return nil
+        }
+        
+        return path
+    }
 }
--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,2 +1,11 @@
 #import "GeneratedPluginRegistrant.h"
+//#import "Cordova/CDV.h"
 #import "ionicAppOne/MainViewController.h"
+#import "Foundation/Foundation.h"
+
+// Defining Category for interface, to avoid duplication
+@interface CDVViewController (IonicIntegration)
+  // Exposing method
+- (NSString*)configFilePath;
+
+@end
